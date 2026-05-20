827e415c405d151467b369107bf2b8198b25d5cf
glam cars <ouaroudabderrahman2020@gmail.com>
2026-05-20 18:32:25 +0100

    edit things

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-base: '/car-rental-app/', 
+base: '/car-rental-app/',
+    build: {
+      modulePreload: false,
+    }, 
    plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
